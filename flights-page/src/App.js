dc555e122e12de8fb55a034f26a450ab041ab737
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-02-08 16:38:37 -0600

    [.] Update App

--- flights-page/src/App.js
+++ flights-page/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Screens/Home/Home';
+import Header from './Screens/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
      <Home/>
     </div>
   );
