23f953b6a442416207a2bbb5cc270b5faf5e911c
AneeOri <81825413+AneeOri@users.noreply.github.com>
2024-02-14 15:57:09 -0600

    [+] Create Form Component

---
+++ flights-page/src/Screens/Form/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useReducer } from "react";
+import { IconPlaneDeparture, IconPlaneArrival, IconCalendarMonth, IconUsersGroup } from '@tabler/icons-react';
+
+export default function Form(){
+
+    return(
+        <div>
+
+        </div>
+    );
+}
